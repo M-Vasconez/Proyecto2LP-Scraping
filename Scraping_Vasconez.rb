47beed7005c06c0a8540a80f3248bb56fe115832
Matias Vasconez <matvguer@espol.edu.ec>
2023-01-20 02:34:17 -0500

    Bug fix scrapping de relojes

--- Scraping_Vasconez.rb
+++ Scraping_Vasconez.rb
@@ -2,7 +2,7 @@ require 'open-uri' # consultar a la plataforma
 require 'nokogiri' # formatear, parsear a html
 require 'csv' # escribir y leer csv
 require 'time' # convierte a tiempo
-
+macro_brands=["Uniden","AcuRite","Spy","Hidden","Indoor","Google","Wireless","Light","Baby","Small","WiFi","FLIPPER"]
 
 CSV.open('relojes.csv', 'wb') do |csv|
   conf=0; pagina=1
@@ -12,14 +12,25 @@ CSV.open('relojes.csv', 'wb') do |csv|
   datosHTML = URI.open(link, 'User-Agent' => 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36')
   parsed_content = Nokogiri::HTML(datosHTML.read)
   parsed_content.css('.sg-col-inner').drop(1).drop(1).drop(1).each do |genre|
-    title = genre.css('.a-size-base-plus').inner_text
+    enunciado = genre.css('.a-size-base-plus').inner_text.split(' ')
     rating = genre.css('.a-icon').css('.a-icon-alt').inner_text[0, 3]
-    price = genre.css('.a-price').css('.a-offscreen').inner_text.split('$')[1]
-    if (!title.empty? and !rating.empty? and !price.to_s.empty?)
-      puts "\n" + title, rating, '$'+price.to_s
-      csv << [title,rating,price]
+    precio = genre.css('.a-price').css('.a-offscreen').inner_text.split('$')[1]
+    if (!enunciado.empty? and !rating.empty? and !precio.to_s.empty?)
+      if(macro_brands.include?enunciado[0])
+        titulo = enunciado[0] + " " + enunciado[1]
+        enunciado.delete_at(0)
+        enunciado.delete_at(0)
+        contenido = enunciado.join(" ")
+      else
+        titulo = enunciado[0]
+        enunciado.delete_at(0)
+        contenido = enunciado.join(" ")
+      end
+      puts "\n" + titulo, contenido, rating, '$'+precio.to_s
+      csv << [titulo, contenido, rating, precio]
     end
     end
   pagina+=1
   end
 end
+
