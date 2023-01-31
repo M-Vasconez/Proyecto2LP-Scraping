4e582293fcbadc5edbc760564906eefd626ae9c2
leowipin <leonardopincayjaramillo@gmail.com>
2023-01-31 17:23:15 -0500

    modificacion en analisis

--- Analisis_Pincay.py
+++ Analisis_Pincay.py
@@ -43,6 +43,9 @@ def ranking_gamas(ram, disco, pulgadas, procesador):
                 break
         new_puntajes.append(new_punt)
     ranks['PUNTUACION'] = new_puntajes
+    ranks = ranks.sort_values(by=['PUNTUACION'])
+    new_index = pd.Index(range(len(ranks)))
+    ranks = ranks.set_index(new_index)
     return ranks
 
 #obteniendo los datos
@@ -53,12 +56,11 @@ laptops = laptops.astype({'PULGADAS':'float'})
 #agregando categorias
 gama = pd.cut(laptops['PRECIO'], 5, labels=['baja','media baja','media','media alta','alta'])
 laptops['GAMA'] = gama
-#grafico de barras precio promedio por marca (PRIMERA PREGUNTA)
+#grafico de barras precio promedio por marca
 df2 = laptops.groupby('MARCA')['PRECIO'].apply(np.mean).reset_index(name="PROMEDIO PRECIO")
 df2.plot.bar(x='MARCA', y='PROMEDIO PRECIO', rot=0)
 plt.savefig('resultados_pincay/precio promedio')
 
-#SEGUNDA PREGUNTA
 #RAM
 df3 = laptops.groupby(['MARCA','RAM'], as_index=False)['PRECIO'].apply(np.mean)
 g=sns.lmplot(x='RAM', y='PRECIO', data=df3, hue='MARCA')
@@ -86,7 +88,6 @@ g.fig.subplots_adjust(top=.95)
 g.ax.set_title('Procesador vs Precio')
 plt.savefig('resultados_pincay/procesador vs precio')
 
-#TERCERA PREGUNTA
 # MEJORES LAPTOPS POR GAMAS QUE TIENEN PROCESADORES INTEL CORE
 laptops_intel= laptops[laptops.PROCESADOR.str.match('intel core i\d$')]
 #GAMA BAJA
@@ -95,9 +96,6 @@ discogb = ranking_feature(laptops_intel, 'DISCO', 'baja')
 pulgadasgb = ranking_feature(laptops_intel, 'PULGADAS', 'baja')
 procesadorgb = ranking_feature(laptops_intel, 'PROCESADOR', 'baja')
 ranksgb = ranking_gamas(ramgb, discogb, pulgadasgb, procesadorgb)
-ranksgb = ranksgb.sort_values(by=['PUNTUACION'])
-new_index = pd.Index(range(len(ranksgb)))
-ranksgb = ranksgb.set_index(new_index)
 headgb = ranksgb.head(10)
 headgb.to_csv('resultados_pincay/ranking gama baja.csv')
 #GAMA MEDIA BAJA
@@ -106,9 +104,6 @@ discogmb = ranking_feature(laptops_intel, 'DISCO', 'media baja')
 pulgadasgmb = ranking_feature(laptops_intel, 'PULGADAS', 'media baja')
 procesadorgmb = ranking_feature(laptops_intel, 'PROCESADOR', 'media baja')
 ranksgmb = ranking_gamas(ramgmb, discogmb, pulgadasgmb, procesadorgmb)
-ranksgmb = ranksgmb.sort_values(by=['PUNTUACION'])
-new_index = pd.Index(range(len(ranksgmb)))
-ranksgmb = ranksgmb.set_index(new_index)
 headgmb = ranksgmb.head(10)
 headgmb.to_csv('resultados_pincay/ranking gama media baja.csv')
 #GAMA MEDIA
@@ -117,9 +112,6 @@ discom = ranking_feature(laptops_intel, 'DISCO', 'media')
 pulgadasm = ranking_feature(laptops_intel, 'PULGADAS', 'media')
 procesadorm = ranking_feature(laptops_intel, 'PROCESADOR', 'media')
 ranksm = ranking_gamas(ramm, discom, pulgadasm, procesadorm)
-ranksm = ranksm.sort_values(by=['PUNTUACION'])
-new_index = pd.Index(range(len(ranksm)))
-ranksm = ranksm.set_index(new_index)
 headm = ranksm.head(10)
 headm.to_csv('resultados_pincay/ranking gama media.csv')
 #GAMA MEDIA ALTA
@@ -128,9 +120,6 @@ discoma = ranking_feature(laptops_intel, 'DISCO', 'media alta')
 pulgadasma = ranking_feature(laptops_intel, 'PULGADAS', 'media alta')
 procesadorma = ranking_feature(laptops_intel, 'PROCESADOR', 'media alta')
 ranksma = ranking_gamas(ramma, discoma, pulgadasma, procesadorma)
-ranksma = ranksma.sort_values(by=['PUNTUACION'])
-new_index = pd.Index(range(len(ranksma)))
-ranksma = ranksma.set_index(new_index)
 headma = ranksma.head(10)
 headma.to_csv('resultados_pincay/ranking gama media alta.csv')
 #GAMA ALTA
@@ -139,8 +128,5 @@ discoga = ranking_feature(laptops_intel, 'DISCO', 'alta')
 pulgadasga = ranking_feature(laptops_intel, 'PULGADAS', 'alta')
 procesadorga = ranking_feature(laptops_intel, 'PROCESADOR', 'alta')
 ranksga = ranking_gamas(ramga, discoga, pulgadasga, procesadorga)
-ranksga = ranksga.sort_values(by=['PUNTUACION'])
-new_index = pd.Index(range(len(ranksga)))
-ranksga = ranksga.set_index(new_index)
 headga = ranksga.head(10)
 headga.to_csv('resultados_pincay/ranking gama alta.csv')
